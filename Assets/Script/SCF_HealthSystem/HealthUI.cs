c86a56314ecdd09c0f0e503b9c2d9d148bd91b55
NapatBuapha <napat.buaph@bumail.net>
2025-03-16 23:42:38 +0700

    Save_16/03/2025
    HeathSystem Added

---
+++ Assets/Script/SCF_HealthSystem/HealthUI.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthUI : MonoBehaviour
+{
+    public int maxHealth = 20; 
+    public int currentHealth;
+    public Image[] hearts;
+    public Sprite fullHeart;  
+    public Sprite halfHeart;  
+    public Sprite emptyHeart; 
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHearts();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHearts();
+    }
+
+    public void Heal(int healAmount)
+    {
+        currentHealth += healAmount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHearts();
+    }
+
+    void UpdateHearts()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            int heartValue = (i + 1) * 2;
+
+            if (currentHealth >= heartValue)
+            {
+                hearts[i].sprite = fullHeart; 
+            }
+            else if (currentHealth == heartValue - 1)
+            {
+                hearts[i].sprite = halfHeart; 
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart; 
+            }
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // กด Spacebar เพื่อลด HP
+        {
+            TakeDamage(2);
+            Debug.Log($"🔥 HP ลดลง! ตอนนี้เหลือ {currentHealth} HP");
+        }
+        if (Input.GetKeyDown(KeyCode.H)) // กด H เพื่อ Heal
+        {
+            Heal(2);
+            Debug.Log($"💖 HP เพิ่มขึ้น! ตอนนี้เหลือ {currentHealth} HP");
+        }
+        if (Input.GetKeyDown(KeyCode.G)) // กด G เพื่อ Heal 1 หน่วย
+        {
+            Heal(1);
+            Debug.Log($"💖 HP เพิ่มขึ้น! ตอนนี้เหลือ {currentHealth} HP");
+        }
+    }
+}
+
--- Assets/Script/SCF_PlayerControl/PlayerController.cs
+++ Assets/Script/SCF_PlayerControl/PlayerController.cs
@@ -1,42 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Callbacks;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
-
-
     //Components
     private Rigidbody rb;
     private RaycastHit hit;
     private Ray groundCheckingRay;
 
-    //Variable [Jump]
-    [Header("[Jump] setting")]
+    //Variable
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private bool isTouchGround;
     [SerializeField] private float checkingDistance = 1f;
+
+    //Object refference
     [SerializeField] private Transform groundCheckRef;
-
-    //Variable [Move]
-    [Header("[Move] setting")]
-    [SerializeField] private float speed = 10f;
-    private Vector3 direction;
-    [SerializeField] private Transform orientation;
-    [SerializeField] private float groundDrag;
-    [SerializeField] private float airDrag;
-
-    [Header("[Gravity] setting")]
-    [SerializeField] private float gravity;
-    [SerializeField] private float airMultiplier;
-    [SerializeField] private float maxFallVelocity;
-    public float velocitycheck;
-
-    
  
     void Awake()
     {
@@ -50,10 +29,9 @@ public class PlayerController : MonoBehaviour
    
     void Update()
     {
-        
-        velocitycheck = rb.velocity.y;
         //Jump System Start
-        if(Input.GetKeyDown(KeyCode.Space) && isTouchGround == true)
+
+        if(Input.GetKey(KeyCode.Space) && isTouchGround == true)
         {
             rb.AddForce(transform.up * jumpForce);
         }
@@ -73,53 +51,12 @@ public class PlayerController : MonoBehaviour
 
         //Jump System end
 
-        //move System Start
-        SpeedControl();
-        float HorizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        direction = orientation.forward * verticalInput + orientation.right * HorizontalInput;
 
 
-        //OnFloor and not condition
-        if(isTouchGround)
-        rb.drag = groundDrag;
-        else
-        rb.drag = airDrag;
+    }
+
     
-        //move System End
-
-    }
-
-    private void FixedUpdate() 
-        {
-            //Gravity
-            if(!isTouchGround)
-            {
-                rb.AddForce(direction.normalized * speed * 10f * airMultiplier, ForceMode.Force);
-                rb.AddForce(transform.up * -gravity);
-            }
-            else
-            rb.AddForce(direction.normalized * speed * 10f , ForceMode.Force);
-        }
-
-    private void SpeedControl()
-    {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        if(flatVel.magnitude > speed)
-        {
-            Vector3 limitedVel = flatVel.normalized * speed;
-            rb.velocity = new Vector3(limitedVel.x , rb.velocity.y , limitedVel.z);
-        }
-
-        if(rb.velocity.y <= -maxFallVelocity)
-        {
-            rb.velocity = new Vector3(rb.velocity.x , -maxFallVelocity , rb.velocity.z);
-        }
-    }
-
-
-
+    
 
 
 }
