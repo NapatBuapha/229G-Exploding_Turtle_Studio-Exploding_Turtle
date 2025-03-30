c2c44b687ad27ab2892a890f78c9baa1e32e5642
NapatBuapha <napat.buaph@bumail.net>
2025-03-30 12:11:52 +0700

    save_30/03/2025
    Full release

--- Assets/Script/SCF_HealthSystem/Player_HealthSystem.cs
+++ Assets/Script/SCF_HealthSystem/Player_HealthSystem.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
-using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class Player_HealthSystem : MonoBehaviour
